a4ad8f785ad40e355bedf6275c01e68835ccb11e
DARLY GORELLE <mrsdianghadg@gmail.com>
2025-05-08 04:02:58 +0100

    modification de migrations

--- database/migrations/2025_05_05_233022_create_transactions_table.php
+++ database/migrations/2025_05_05_233022_create_transactions_table.php
@@ -11,16 +11,18 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('transactions', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('order_id')->constrained('orders')->cascadeOnDelete();
-            $table->foreignId('provider')->nullable();
-            $table->string('reference');
-            $table->string('status')->default('pending');
-            $table->string('amount');
-            $table->string('currency');           
-            $table->timestamps();
-            $table->json('metadata')->nullable();
+        Schema::withoutForeignKeyConstraints(function () {
+            Schema::create('transactions', function (Blueprint $table) {
+                $table->id();
+                $table->foreignId('order_id')->constrained('orders')->cascadeOnDelete();
+                $table->foreignId('provider')->nullable();
+                $table->string('reference');
+                $table->string('status')->default('pending');
+                $table->string('amount');
+                $table->string('currency');           
+                $table->timestamps();
+                $table->json('metadata')->nullable();
+            });
         });
     }
 
